985b6ec9457dc0a507be3b0766a0f5e333d21060
Jopus <24100896@usc.edu.ph>
2025-07-06 05:38:42 +0800

    Organized Files

--- OrderTracker_AquaSol/js/signup.js
+++ OrderTracker_AquaSol/js/signup.js
@@ -2,7 +2,7 @@ var formBody = document.getElementById("signupForm");
 formBody.addEventListener("submit", function(event){
     event.preventDefault();
     var data = new FormData(formBody);
-    fetch("signup.php", {
+    fetch("../php/signup.php", {
         method: 'POST',
         body: data
     }).
