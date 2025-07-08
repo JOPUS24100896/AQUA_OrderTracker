58536c4ed7d7674a342b48c2a5be5a128492708d
Jopus <24100896@usc.edu.ph>
2025-07-08 16:18:18 +0800

    Updated database and renamed variables

--- php/login.php
+++ php/login.php
@@ -4,8 +4,9 @@ $conn = new mysqli("localhost", "root", "", "aquadelsol_ordertracker");
 $login_key = $_POST["login_key"];
 $pass_key = $_POST["password"];
 
-$verify_query = "SELECT verify_login(?, ?) as user_id";
+$verify_query = "CALL verify_login(?, ?)";
 $verify_prep = $conn->prepare($verify_query);
+$verify_prep->bind_param("ss", $login_key, $pass_key);
 if(!$verify_prep){
     echo json_encode([
         "Error" => true,
@@ -18,7 +19,7 @@ $verify_prep->bind_param("ss", $login_key, $pass_key);
 if($verify_prep->execute()){
     $verify_raw = $verify_prep->get_result();
     $verify_assoc = $verify_raw->fetch_assoc();
-    if($verify_assoc["user_id"] == null){
+    if($verify_assoc["UserID"] == null){
         echo json_encode([
             "Error" => true,
             "Error_Number" => $verify_prep->errno,
@@ -27,9 +28,11 @@ if($verify_prep->execute()){
     }else{
         echo json_encode([
             "Error" => false,
-            "ID" => $verify_assoc["user_id"] //Temporary debugging 
+            "ID" => $verify_assoc["UserID"], //Temporary debugging 
+            "Type" => $verify_assoc["Type"]
         ]);
-        $_SESSION["user_id"] = $verify_assoc["user_id"];
+        $_SESSION["user_id"] = $verify_assoc["UserID"];
+        $_SESSION["user_type"] = $verify_assoc["Type"];
     }
 }
 else echo json_encode([
