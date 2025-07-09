7f3b43d2075fa9dadc6b02173370d67c028c9d06
Rinald <168900216+Webisekai20@users.noreply.github.com>
2025-07-09 18:00:22 +0800

    Major Update!!!!

--- js/graphFunction.js
+++ js/graphFunction.js
@@ -31,7 +31,7 @@ const config = {
         stacked: false,
         plugins: {
             title: {
-                display: true,
+                display: false,
                 text: 'Business Report'
             }
         },
