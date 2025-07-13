5368329a770fd8bf5ed19599cb85f4c21881cd20
JOPUS24100896 <24100896@usc.edu.ph>
2025-07-13 23:54:29 +0800

    Added .catch for error checking

--- js/orderHistory.js
+++ js/orderHistory.js
@@ -2,6 +2,7 @@ fetch("../php/retrieve_orderData.php")
 .then(response => response.json())
 .then(data => {
     const tableRow = document.getElementById("orderForm");
+    console.log(data);
     data.forEach(order => {
         const card = document.createElement('tr');
         card.innerHTML = `
@@ -20,6 +21,8 @@ fetch("../php/retrieve_orderData.php")
     //         <td>${data.Price}</td>
     //     `;
     //     tableRow.appendChild(card);
+}).catch(data => {
+    console.log(data);
 });
 
 
@@ -42,7 +45,7 @@ fetch("../php/retrieve_orderData.php")
 fetch("../php/test.json")
 .then(response => response.json())
 .then(data => {
-    orderData = data;
+    order = data;
     const tableRow = document.getElementById("orderForm");
     data.forEach(order => {
         const card = document.createElement('tr');
@@ -54,6 +57,8 @@ fetch("../php/test.json")
         `;
         tableRow.appendChild(card);
     });
+}).catch(data => {
+    console.log(data);
 });
 // testing
 
