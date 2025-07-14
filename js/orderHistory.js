8c1c6f85b24f2446bd246cef47c3aaa136a4ac54
JOPUS24100896 <24100896@usc.edu.ph>
2025-07-14 17:30:37 +0800

    Created Order history list ordered by OrderID

--- js/orderHistory.js
+++ js/orderHistory.js
@@ -4,18 +4,31 @@ fetch("../php/order_history.php").
 then(response => response.json()). 
 then(data => {
     let row = "";
+    let span_count = 0;
     console.log(data);
     data.forEach(rowData => {
         row += `
-        <tr>
-            <td>${rowData.UserID}</td>
-            <td>${rowData.OrderID}</td>
-            <td>${rowData.ItemID}</td>
-            <td>${rowData.ItemQuantity}</td>
-            <td>${rowData.OrderDate}</td>
-            <td>${rowData.TotalPrice}</td>
+        <tr class="orderRow">
+            <td class="orderData OrderId${rowData.OrderID}">${rowData.OrderID}</td>
+            <td class="orderData">${rowData.ItemID}</td>
+            <td class="orderData">${rowData.ItemQuantity}</td>
+            <td class="orderData">${rowData.OrderDate}</td>
+            <td class="orderData">${rowData.TotalPrice}</td>
         </tr>
     `;
     })
     table.innerHTML = row;
+    data.forEach(row => {
+        const Col = document.getElementsByClassName("OrderId" + row.OrderID);
+        console.log(Col);
+        console.log("OrderId" + row.OrderID)
+        if(span_count == 0){
+            span_count = Col.length - 1;
+            Col[0].rowSpan = span_count + 1;
+        }else{
+            Col[Col.length - span_count].remove();
+            span_count--;
+        }
+            
+    });
 });
