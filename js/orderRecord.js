1d703a078be3e9f7067a8ec0d7a4aff06eb4bca4
Rinald <168900216+Webisekai20@users.noreply.github.com>
2025-07-14 15:17:57 +0800

    update

--- js/orderRecord.js
+++ js/orderRecord.js
@@ -13,14 +13,6 @@ fetch("../php/retrieve_orderData.php")
         `;
         tableRow.appendChild(card);
     });
-    // const card = document.createElement('tr');
-    //     card.innerHTML = `
-    //         <td>${data.OrderID}</td>
-    //         <td>${data.FullName}</td>
-    //         <td>${data.OrderDate}</td>
-    //         <td>${data.Price}</td>
-    //     `;
-    //     tableRow.appendChild(card);
 }).catch(data => {
     console.log(data);
 });
@@ -32,32 +24,22 @@ fetch("../php/retrieve_orderData.php")
 
 
 // testing
-// const destinationGrid = document.getElementById("orderForm");
-// const card = document.createElement('tr');
-// card.innerHTML = `
-//     <td>1</td>
-//         <td>John Doe</td>
-//         <td>2023-10-01</td>
-//         <td>Completed</td>
-// `;
-// destinationGrid.appendChild(card);
-    
-fetch("../php/test.json")
-.then(response => response.json())
-.then(data => {
-    orderData = data;
-    const tableRow = document.getElementById("orderForm");
-    data.forEach(order => {
-        const card = document.createElement('tr');
-        card.innerHTML = `
-            <td>${order.OrderID}</td>
-            <td>${order.FullName}</td>
-            <td>${order.OrderDate}</td>
-            <td>${order.Price}</td>
-        `;
-        tableRow.appendChild(card);
-    });
-});
+// fetch("../php/test.json")
+// .then(response => response.json())
+// .then(data => {
+//     orderData = data;
+//     const tableRow = document.getElementById("orderForm");
+//     data.forEach(order => {
+//         const card = document.createElement('tr');
+//         card.innerHTML = `
+//             <td>${order.OrderID}</td>
+//             <td>${order.FullName}</td>
+//             <td>${order.OrderDate}</td>
+//             <td>${order.Price}</td>
+//         `;
+//         tableRow.appendChild(card);
+//     });
+// });
 // testing
 
 
