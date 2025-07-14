f526793cbd38f2ccdd7c61e0fa8c3bb23363ca66
Rinald <168900216+Webisekai20@users.noreply.github.com>
2025-07-14 16:39:02 +0800

    update

--- js/orderRecord.js
+++ js/orderRecord.js
@@ -13,34 +13,7 @@ fetch("../php/retrieve_orderData.php")
         `;
         tableRow.appendChild(card);
     });
-}).catch(data => {
-    console.log(data);
-});
-
-
-
-
-
-
-
-// testing
-// fetch("../php/test.json")
-// .then(response => response.json())
-// .then(data => {
-//     orderData = data;
-//     const tableRow = document.getElementById("orderForm");
-//     data.forEach(order => {
-//         const card = document.createElement('tr');
-//         card.innerHTML = `
-//             <td>${order.OrderID}</td>
-//             <td>${order.FullName}</td>
-//             <td>${order.OrderDate}</td>
-//             <td>${order.Price}</td>
-//         `;
-//         tableRow.appendChild(card);
-//     });
-// });
-// testing
+})
 
 
 
--- php/retrieve_orderData.php
+++ php/retrieve_orderData.php
@@ -1,78 +1,6 @@
 <?php
     session_start();
-    // class Item {
-    //     public $ItemID;
-    //     public $ItemName;
-    //     public $Unit_price;
-    //     // methods
-    //     function set_ItemId($Id) {
-    //         $this->ItemID = $Id;
-    //     }
-    //     function set_Unit_price($Price) {
-    //         $this->Unit_price = $Price;
-    //     }
-    // }
-    // $Item1 = new Item(); $Item1->set_ItemId(1);
-    // $Item2 = new Item(); $Item2->set_ItemId(2);
-    // $Item3 = new Item(); $Item3->set_ItemId(3);
-    // $UnitPrice_arr = array($Item1, $Item2, $Item3);
-
-
-    // class OrderDetails {
-    //     public $ItemID;
-    //     public $Quantity;
-    //     public $TotalPrice;
-
-    //     // methods
-    //     function set_ItemId($Id) {
-    //         $this->ItemID = $Id;
-    //     }
-    //     function set_Quantity($Qty) {
-    //         $this->Quantity = $Qty;
-    //     }
-    //     function calculate_Price($UnitPrice_arr){
-    //         for ($i=0; $i < count($UnitPrice_arr); $i++) { 
-    //             if(strcmp($this->ItemID, $UnitPrice_arr[$i]) == 0){
-    //                 $this->TotalPrice =  $this->Quantity * $UnitPrice_arr[$i]->Unit_price; 
-    //                 //recheck if UnitPrice_arr accessing is correct----------------
-    //             };
-    //         }
-    //         $this->TotalPrice = $this->calculate_Price($UnitPrice_arr);
-    //     }
-    // }
-    // class Order_Profile {
-    // // Properties
-    //     public $OrderID;
-    //     public $FullName;
-    //     public $OrderDate;
-    //     public $OrderDetails = []; //Array of OrderDetails objects
-    //     public $TotalAmount;
-
-    //     // Methods
-    //     function set_OrderId($id){
-    //         $this->OrderID = $id;
-    //     }
-    //     function set_FullName($name){
-    //         $this->FullName = $name;
-    //     }
-    //     function set_OrderDate($date){
-    //         $this->OrderDate = $date;
-    //     }
-    //     function set_TotalAmount($total){
-    //         $this->TotalAmount = $total;
-    //     }
-    // }
-
-
-
-    // k shjghflgfahwgbfuftgb8fy4qcr
-
-
     $conn = new mysqli("localhost", "root", "", "aquadelsol_ordertracker");
-    // Queries
-    // $sql1 = "SELECT `items`.`ItemID`, `items`.`ItemName`, `items`.`Price`
-    //         FROM `items`
-    //         ORDER BY `items`.`ItemID`;";
     $sql2 = "SELECT `order_details`.`OrderID`, `users`.`FullName`, `order_details`.`ItemID`, `order_details`.`ItemQuantity`, `orders`.`OrderDate`, `items`.`Price`
             FROM `order_details`
             INNER JOIN `items`
@@ -82,14 +10,10 @@
             INNER JOIN `users`
             ON `orders`.`UserID` = `users`.`UserID`
             ORDER By `order_details`.`OrderID`, `order_details`.`ItemID`;";
-
-    //get data
-    // $raw_Item_Data = $conn->query($sql1);
     $raw_Order_Data = $conn->query($sql2);
     $json_placeholder = [];
     while($query_raw_row = $raw_Order_Data->fetch_assoc()){
         $json_placeholder[] = $query_raw_row;
     }
-    
     echo json_encode($json_placeholder);
 ?>
