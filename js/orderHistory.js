12c116d458dd83edb2d127b99845f9e9681d2b59
JOPUS24100896 <24100896@usc.edu.ph>
2025-07-14 17:41:51 +0800

    Formatted order history list

--- js/orderHistory.js
+++ js/orderHistory.js
@@ -7,26 +7,34 @@ then(data => {
     let span_count = 0;
     console.log(data);
     data.forEach(rowData => {
+        let itemQuantPrice = parseFloat(rowData.ItemQuantity) * rowData.Price;
         row += `
         <tr class="orderRow">
             <td class="orderData OrderId${rowData.OrderID}">${rowData.OrderID}</td>
-            <td class="orderData">${rowData.ItemID}</td>
+            <td class="orderData">${rowData.ItemName}</td>
             <td class="orderData">${rowData.ItemQuantity}</td>
-            <td class="orderData">${rowData.OrderDate}</td>
-            <td class="orderData">${rowData.TotalPrice}</td>
+            <td class="orderData">${itemQuantPrice}</td>
+            <td class="orderData OrderDate${rowData.OrderID}">${rowData.OrderDate}</td>
+            <td class="orderData OrderPrice${rowData.OrderID}">${rowData.TotalPrice}</td>
         </tr>
     `;
     })
     table.innerHTML = row;
     data.forEach(row => {
-        const Col = document.getElementsByClassName("OrderId" + row.OrderID);
-        console.log(Col);
+        const ColID = document.getElementsByClassName("OrderId" + row.OrderID);
+        const ColDate = document.getElementsByClassName("OrderDate" + row.OrderID);
+        const ColPrice = document.getElementsByClassName("OrderPrice" + row.OrderID);
+        console.log(ColID);
         console.log("OrderId" + row.OrderID)
         if(span_count == 0){
-            span_count = Col.length - 1;
-            Col[0].rowSpan = span_count + 1;
+            span_count = ColID.length - 1;
+            ColID[0].rowSpan = span_count + 1;
+            ColDate[0].rowSpan = span_count + 1;
+             ColPrice[0].rowSpan = span_count + 1;
         }else{
-            Col[Col.length - span_count].remove();
+            ColID[ColID.length - span_count].remove();
+            ColDate[ColDate.length - span_count].remove();
+            ColPrice[ColPrice.length - span_count].remove();
             span_count--;
         }
             
