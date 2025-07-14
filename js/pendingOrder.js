ec62e045cafccc9dbfdce83107f4ae07f0fea2c0
JOPUS24100896 <24100896@usc.edu.ph>
2025-07-15 05:47:59 +0800

    Updated Pending_orders

--- js/pendingOrder.js
+++ js/pendingOrder.js
@@ -12,31 +12,50 @@ then(data => {
         <tr class="orderRow">
             <td class="orderData OrderId${rowData.OrderID}">${rowData.OrderID}</td>
             <td class="orderData">${rowData.ItemName}</td>
-            <td class="orderData">${rowData.ItemQuantity}</td>
             <td class="orderData">${itemQuantPrice}</td>
             <td class="orderData OrderDate${rowData.OrderID}">${rowData.OrderDate}</td>
             <td class="orderData OrderPrice${rowData.OrderID}">${rowData.TotalPrice}</td>
+            <td class="orderData OrderStat${rowData.OrderID}">${rowData.Status}</td>
         </tr>
     `;
-    })
+    });
     table.innerHTML = row;
-    data.forEach(row => {
-        const ColID = document.getElementsByClassName("OrderId" + row.OrderID);
-        const ColDate = document.getElementsByClassName("OrderDate" + row.OrderID);
-        const ColPrice = document.getElementsByClassName("OrderPrice" + row.OrderID);
+    filter();
+    data.forEach(rowData => {
+        const ColID = document.getElementsByClassName("OrderId" + rowData.OrderID);
+        const ColDate = document.getElementsByClassName("OrderDate" + rowData.OrderID);
+        const ColPrice = document.getElementsByClassName("OrderPrice" + rowData.OrderID);
+        const ColStat = document.getElementsByClassName("OrderStat" + rowData.OrderID);
         console.log(ColID);
-        console.log("OrderId" + row.OrderID)
+        console.log("OrderId" + rowData.OrderID)
         if(span_count == 0){
             span_count = ColID.length - 1;
             ColID[0].rowSpan = span_count + 1;
             ColDate[0].rowSpan = span_count + 1;
-             ColPrice[0].rowSpan = span_count + 1;
+            ColPrice[0].rowSpan = span_count + 1;
+            ColStat[0].rowSpan = span_count + 1;
         }else{
             ColID[ColID.length - span_count].remove();
             ColDate[ColDate.length - span_count].remove();
             ColPrice[ColPrice.length - span_count].remove();
+            ColStat[ColStat.length - span_count].remove();
             span_count--;
         }
             
     });
+
+    //filter();
 });
+
+function filter() {
+    var row, column, i;
+    row = table.getElementsByTagName("tr");
+    for (i = 0; i < row.length; i++) {
+        column = row[i].getElementsByTagName("td")[5];
+        if(column.innerHTML != 'Complete') {
+            row[i].style.display = "";
+        }else {
+            row[i].style.display = "none";
+        }       
+    }
+}
